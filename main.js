5c1012bcf0db298adb926cc20143c5f84fa026f6
Umairshaikh3434 <160262401+Umairshaikh3434@users.noreply.github.com>
2024-03-22 05:58:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// BMI Calculator
+let weight = 70;
+let height = 2;
+let bmi = weight / (height * height);
+console.log(`umair your BMI is ${bmi}`);
+export {};
---
+++ main.ts
@@ -0,0 +1,8 @@
+// BMI Calculator
+
+let weight = 70;
+let height = 2;
+
+let bmi = weight/ (height*height)
+
+console.log(`umair your BMI is ${bmi}`)
